44f9cf1c7554008a9bc41df1c02356321121fbe8
Fiskless <Fiskless@gmail.com>
2022-02-11 18:53:50 +0500

    Change file

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ class S(BaseHTTPRequestHandler):
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
 
-def run(server_class=HTTPServer, handler_class=S, port=8000):
+def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
